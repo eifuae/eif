42ba46121ca0ac79644416735eaf309f94453106
eifuae <ahmed@eif.gov.ae>
2024-12-18 02:14:51 +0400

    Deleting bin bash Script to schedule.sh

--- bin bash Script to schedule.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to schedule shutdown at 22:40 daily
-
-# Schedule the shutdown
-echo "40 22 * * * /sbin/shutdown -h now" | crontab -
-
-echo "Shutdown scheduled daily at 22:40."
-
